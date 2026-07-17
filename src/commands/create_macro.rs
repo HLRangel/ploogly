c6ab551823bd480eea68d7ce89a16b2b6f3952e4
HLRangel <rangel.receiving@comrades.sbs>
2026-07-16 21:01:06 -0300

    Start adding create_macro

--- src/commands/create_macro.rs
+++ src/commands/create_macro.rs
@@ -1,17 +1,23 @@
 use crate::interpreter_facilities::*;
 use std::collections::HashMap;
 
+/* How macros work in Ploogly:
+
+    ![ key_name ]! gets replaced by key;
+*/
+
+// Just pushes inner content as a variable
 pub fn create_macro(
     origin: &[u8],
     current: &mut usize,
     last: &mut usize,
-    _vars: &mut HashMap<String, Vec<u8>>,
-    _anon_stack: &mut Vec<Vec<u8>>,
+    vars: &mut HashMap<String, Vec<u8>>,
+    anon_stack: &mut Vec<Vec<u8>>,
 ) -> Result<Vec<u8>, std::io::Error> {
-    let params: Vec<Vec<u8>> = read_params_until_nl(origin, last, current)?;
+    let macro_name: String = get_worl_produce_st(origin, current, last, vars, anon_stack)?;
     let macro_content: Vec<u8> = get_inner(origin, last, current)?;
-    
-    println!("{:?} {}", params, String::from_utf8(macro_content).unwrap());
+
+    vars.insert(format!("__macro+{macro_name}"), macro_content);
     
     return Ok(Vec::new());
 }
